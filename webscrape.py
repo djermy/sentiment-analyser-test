bc7ad0254ab7b97df2ae94ac686dcb62c072a2b0
djermy <danjermy@yahoo.com>
2023-01-17 12:09:29 +0100

    created make_df_lists() to make lists ready to be turned into pandas dataframe

--- webscrape.py
+++ webscrape.py
@@ -9,26 +9,34 @@ time.sleep(3)
 driver.find_element_by_id('consent_prompt_submit').click()
 driver.find_element_by_id('reviews-accordion-accordion-control-reviews-accordion').click()
 show_more_button = None
+
 try:
     show_more_button = driver.find_element_by_css_selector('[data-test="show-x-more-reviews-button"]')
 except:
     show_more_button = None
+
 while show_more_button:
     show_more_button.click()
-    time.sleep(0.4)
+    time.sleep(0.4)  
     try:
         show_more_button = driver.find_element_by_css_selector('[data-test="show-x-more-reviews-button"]')
     except:
         show_more_button = None
+        
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 driver.quit()
 
 review_container = soup.find("div", {"class": "Reviewsstyles__ReviewsContainer-sc-6g3q7a-10"})
 reviews = review_container.find_all('div', {'data-test': 'review-item'})
-for review in reviews:
-    title = review.find('p', {'class': 'ReviewItemstyle__Title-sc-8dsnp1-0'}).decode_contents()
-    description = review.find('p', {'itemprop': 'reviewBody'}).decode_contents()
-    rating = review.find('span', {'itemprop': 'ratingValue'}).decode_contents()
-    print(f'Title: {title}\nDescription: {description}\nRating: {rating} out of 5\n---')
-
 print('Total Reviews: ' + str(len(reviews)))
+print(type(reviews))
+def make_df_lists(reviews):
+    '''
+    Takes 
+    '''
+
+    # turning reviews into seperate lists for 
+    titles = [review.find('p', {'class': 'ReviewItemstyle__Title-sc-8dsnp1-0'}).decode_contents() for review in reviews]
+    descriptions = [review.find('p', {'itemprop': 'reviewBody'}).decode_contents() for review in reviews]
+    ratings = [review.find('span', {'itemprop': 'ratingValue'}).decode_contents() for review in reviews]
+    return titles, descriptions, ratings
