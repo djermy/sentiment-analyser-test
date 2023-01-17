ed06431dca8fa43d97ecd4cf913015a76ef86931
djermy <danjermy@yahoo.com>
2023-01-17 12:11:31 +0100

    resaved

--- webscrape.py
+++ webscrape.py
@@ -29,14 +29,15 @@ driver.quit()
 review_container = soup.find("div", {"class": "Reviewsstyles__ReviewsContainer-sc-6g3q7a-10"})
 reviews = review_container.find_all('div', {'data-test': 'review-item'})
 print('Total Reviews: ' + str(len(reviews)))
-print(type(reviews))
+
 def make_df_lists(reviews):
     '''
-    Takes 
+    Takes reviews BeautifulSoup element and returns 3 lists: titles, descriptions, ratings.
     '''
 
     # turning reviews into seperate lists for 
     titles = [review.find('p', {'class': 'ReviewItemstyle__Title-sc-8dsnp1-0'}).decode_contents() for review in reviews]
     descriptions = [review.find('p', {'itemprop': 'reviewBody'}).decode_contents() for review in reviews]
     ratings = [review.find('span', {'itemprop': 'ratingValue'}).decode_contents() for review in reviews]
+    
     return titles, descriptions, ratings
