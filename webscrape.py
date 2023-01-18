c877242941f651d9aa7459533a1a12c20db0806f
djermy <danjermy@yahoo.com>
2023-01-18 10:17:02 +0100

    updated make_df_lists() to generate and return dataframe instead of returning lists needed to build it

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import pandas as pd
 
 driver = webdriver.Firefox()
 url = "https://www.argos.co.uk/product/2010517"
@@ -8,6 +9,8 @@ driver.get(url)
 time.sleep(3)
 driver.find_element_by_id('consent_prompt_submit').click()
 driver.find_element_by_id('reviews-accordion-accordion-control-reviews-accordion').click()
+show_more_button = None
+
 show_more_button = None
 
 try:
@@ -30,14 +33,21 @@ review_container = soup.find("div", {"class": "Reviewsstyles__ReviewsContainer-s
 reviews = review_container.find_all('div', {'data-test': 'review-item'})
 print('Total Reviews: ' + str(len(reviews)))
 
-def make_df_lists(reviews):
+def generate_dataframe(reviews):
     '''
-    Takes reviews BeautifulSoup element and returns 3 lists: titles, descriptions, ratings.
+    Takes reviews BeautifulSoup element and returns dataframe with 3 columns:
+    title, description, rating.
     '''
 
-    # turning reviews into seperate lists for 
+    # turning reviews into seperate lists for the dataframe
     titles = [review.find('p', {'class': 'ReviewItemstyle__Title-sc-8dsnp1-0'}).decode_contents() for review in reviews]
     descriptions = [review.find('p', {'itemprop': 'reviewBody'}).decode_contents() for review in reviews]
     ratings = [review.find('span', {'itemprop': 'ratingValue'}).decode_contents() for review in reviews]
     
-    return titles, descriptions, ratings
+    # build the dataframe
+    df = pd.DataFrame(
+        list(zip(titles, descriptions, ratings)),
+        columns=['title', 'description', 'rating'])
+
+    return df
+print(generate_dataframe(reviews))
